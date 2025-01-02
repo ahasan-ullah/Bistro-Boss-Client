c226ec6f6c11b5b1a750fe9180d39a4bb44eb02b
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-02 23:51:56 +0600

    homepage router added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
 
 const router=createBrowserRouter([
   {
     path: '/',
-    element: <h1>Hello</h1>
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   }
 ])
 export default router;
