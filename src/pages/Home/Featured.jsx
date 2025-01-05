f9dc978e27cd4aadbba4e621710bf6dbfd8f8beb
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-06 02:03:54 +0600

    added parallex to featured section

--- src/pages/Home/Featured.jsx
+++ src/pages/Home/Featured.jsx
@@ -4,17 +4,17 @@ import './Featured.css'
 
 const Featured = () => {
   return (
-    <div className="featured-item text-white pt-8 my-10">
+    <div className="featured-item bg-fixed text-white pt-8 my-10">
       <SectionTitle heading={"Featured Items"} subHeading={"Check It Out"}></SectionTitle>
-      <div className="md:flex justify-center items-center pt-12 pb-20 px-36">
+      <div className="md:flex justify-center items-center pt-12 pb-20 px-36 bg-slate-500 bg-opacity-60">
         <div>
           <img src={featuredImage} alt="" />
         </div>
-        <div className="md:ml-10">
+        <div className="md:ml-10 space-y-3">
           <p>Aug 20, 2025</p>
           <p className="uppercase">Where can i get some?</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure aperiam, ipsa doloribus ex architecto provident temporibus eaque eligendi nesciunt. Delectus, deleniti saepe ipsam eius sit voluptates, accusantium provident laboriosam quis aliquid dolorum excepturi? Sit, sed rerum dolorem ratione repudiandae quisquam necessitatibus, sequi nulla vitae ipsa, incidunt aliquam quia maxime.</p>
-          <button className="btn btn-outline">Read More</button>
+          <button className="btn btn-outline border-0 border-b-4">Read More</button>
         </div>
       </div>
     </div>
