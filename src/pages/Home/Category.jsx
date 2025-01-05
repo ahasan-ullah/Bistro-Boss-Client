5b2ab951ba7117ed3b9d381e0f2cfb83d4b14dc0
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-06 01:26:18 +0600

    dynamic section added

--- src/pages/Home/Category.jsx
+++ src/pages/Home/Category.jsx
@@ -8,10 +8,12 @@ import slide2 from '../../assets/home/slide2.jpg'
 import slide3 from '../../assets/home/slide3.jpg'
 import slide4 from '../../assets/home/slide4.jpg'
 import slide5 from '../../assets/home/slide5.jpg'
+import SectionTitle from '../../components/SectionTitle';
 
 const Category = () => {
   return (
-    <div>
+    <section>
+      <SectionTitle heading={"Order Online"} subHeading={"From 1.00am to 10.00pm"}></SectionTitle>
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -43,7 +45,7 @@ const Category = () => {
           <h3 className='text-4xl uppercase text-center -mt-16 text-white'>SALADS</h3>
         </SwiperSlide>
       </Swiper>
-    </div>
+    </section>
   );
 };
 
