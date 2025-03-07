5bc651172f26f54b2032f50d276d7d84245cf7c3
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-08 00:27:12 +0600

    My cart page total price calculated

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,7 +1,16 @@
+import useCart from "../../../hooks/useCart";
+
 const Cart = () => {
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
-    <div >
-      <h2 className="text-4xl">My Cart</h2>
+    <div>
+      <div className="flex justify-evenly">
+        <h2 className="text-4xl">Items: {cart.length}</h2>
+        <h2 className="text-4xl">Total Price: {totalPrice}</h2>
+        <button className="btn btn-primary">Pay</button>
+      </div>
+      
     </div>
   );
 };
