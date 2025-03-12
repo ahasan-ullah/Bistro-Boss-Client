48ba98acf1315c9aab7881f686a174382d8ecff2
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-13 00:21:42 +0600

    added interceptors to axiosSecure hook

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,20 @@ const axiosSecure=axios.create({
   baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(function(config){
+    const token=localStorage.getItem('access-token');config.headers.authorization=`Bearer ${token}`;
+    return config;
+  },function(error){
+    return Promise.reject.error;
+  })
+
+  axiosSecure.interceptors.response.use(function(response){
+    return response;
+  },function(error){
+    const status=error.response.status;
+    return Promise.reject(error);
+  })
+
   return axiosSecure;
 };
 
