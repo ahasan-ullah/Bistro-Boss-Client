c417c8d72068b29df33611b52bd831e250f6f766
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-12 23:49:31 +0600

    added token jwt token to local storage while logging

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import {app} from "../firebase/firebase.config"
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const authContext=createContext(null);
 const auth=getAuth(app);
@@ -8,6 +9,7 @@ const auth=getAuth(app);
 const AuthProvider = ({children}) => {
   const [user,setUser]=useState(null);
   const [loading,setLoading]=useState(true);
+  const axiosPublic=useAxiosPublic();
 
   const googleProvider=new GoogleAuthProvider();
 
@@ -40,6 +42,18 @@ const AuthProvider = ({children}) => {
   useEffect(()=>{
     const unsubscribe=onAuthStateChanged(auth,currentUser=>{
       setUser(currentUser);
+      if(currentUser){
+        const userInfo={email: currentUser.email};
+        axiosPublic.post('/jwt',userInfo)
+        .then(res=>{
+          if(res.data.token){
+            localStorage.setItem('access-token', res.data.token);
+          }
+        })
+      }
+      else{
+        localStorage.removeItem('access-token');
+      }
       setLoading(false);
     });
     return ()=>{
