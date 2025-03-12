49949374e375a32ef4377f5404361f65ea5a79cb
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-13 00:28:10 +0600

    modified the previous commit

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure=axios.create({
   baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+  const navigate=useNavigate();
+  const {logout}=useAuth();
+
   axiosSecure.interceptors.request.use(function(config){
     const token=localStorage.getItem('access-token');config.headers.authorization=`Bearer ${token}`;
     return config;
@@ -13,8 +18,12 @@ const useAxiosSecure = () => {
 
   axiosSecure.interceptors.response.use(function(response){
     return response;
-  },function(error){
+  },async function(error){
     const status=error.response.status;
+    if(status===401 || status===403){
+      await logout();
+      navigate('/login')
+    }
     return Promise.reject(error);
   })
 
