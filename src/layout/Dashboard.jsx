c2e90200e078a1ada3fbf3d89951383ebec862d9
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-13 14:02:07 +0600

    custom hook added to check wheteher user is admin or not

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -13,11 +13,12 @@ import {
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 const Dashboard = () => {
   const [cart] = useCart();
 
-  const isAdmin = true;
+  const [isAdmin] = useAdmin();
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
