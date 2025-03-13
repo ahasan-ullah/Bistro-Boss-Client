83a158ee309ab7a489a2669b8e9c20c7064445df
Ahasan Ullah <ahasanullah451@gmail.com>
2025-03-13 14:35:37 +0600

    Admin route secured

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,17 +1,18 @@
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const PrivateRoute = ({children}) => {
+const AdminRoutes = ({children}) => {
   const {user,loading}=useAuth();
+  const [isAdmin,isAdminLoading]=useAdmin();
   const location=useLocation();
-
-  if(loading){
+  if(loading || isAdminLoading){
     return <progress className="progres w-56"></progress>
   }
-  if(user){
+  if(user && isAdmin){
     return children;
   }
   return <Navigate state={{from:location}} replace to={"/login"}></Navigate>
 };
 
-export default PrivateRoute;
+export default AdminRoutes;
