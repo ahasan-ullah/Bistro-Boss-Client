9acb85fb350b22d76bf3281f0a9e9be55da77c2f
Ahasan Ullah <ahasanullah451@gmail.com>
2025-04-03 00:06:23 +0600

    conditional dashboard button added to the navbar

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { authContext } from "../../providers/AuthProvider";
 import { FaShoppingCart } from "react-icons/fa";
 import useCart from "../../hooks/useCart";
+import useAdmin from "../../hooks/useAdmin";
 
 const Navbar = () => {
   const { user, logout } = useContext(authContext);
-  const [cart]=useCart();
+  const [isAdmin] = useAdmin();
+  const [cart] = useCart();
   const handleLogout = () => {
     logout()
       .then(() => {})
@@ -23,9 +25,16 @@ const Navbar = () => {
       <li>
         <Link to={"/order/salad"}>Order</Link>
       </li>
-      <li>
-        <Link to={"/secret"}>Secret</Link>
-      </li>
+      {user && isAdmin && (
+        <li>
+          <Link to={"/dashboard/adminHome"}>Dashboard</Link>
+        </li>
+      )}
+      {user && !isAdmin && (
+        <li>
+          <Link to={"/dashboard/userHome"}>Dashboard</Link>
+        </li>
+      )}
       <li>
         <Link to={"dashboard/cart"}>
           <button className="btn">
