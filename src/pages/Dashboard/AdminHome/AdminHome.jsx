591ff635ea914a5cf2852c8d98605865b69a94f2
Ahasan Ullah <ahasanullah451@gmail.com>
2025-04-08 01:24:45 +0600

    barchart added

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -3,24 +3,48 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaBook, FaDollarSign } from "react-icons/fa";
 import { FaJediOrder, FaUser } from "react-icons/fa6";
+import React from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 const AdminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
   const { data: stats } = useQuery({
-    queryKey: ["admin-stats"],
+    queryKey: ["admin-chartData"],
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
       return res.data;
     },
   });
-  const {data: chartData}=useQuery({
-    queryKey: ['order-stats'],
-    queryFn: async()=>{
-      const res=await axiosSecure.get('/order-stats');
+
+  const { data: chartData=[] } = useQuery({
+    queryKey: ["order-chartData"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/order-stats");
       return res.data;
-    }
-  })
+    },
+  });
+
+  
+  console.log(chartData)
+  console.log(chartData)
+  
+
+  //custom chart shape
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+  
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+  
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
   return (
     <div>
       <h2 className="text-3xl">
@@ -32,7 +56,7 @@ const AdminHome = () => {
             <FaDollarSign className="h-8 w-8"></FaDollarSign>
           </div>
           <div className="stat-title">Revenue</div>
-          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-value">${chartData?.revenue}</div>
           <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
 
@@ -41,7 +65,7 @@ const AdminHome = () => {
             <FaUser className="h-8 w-8"></FaUser>
           </div>
           <div className="stat-title">Users</div>
-          <div className="stat-value">{stats?.user}</div>
+          <div className="stat-value">{chartData?.user}</div>
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
@@ -50,19 +74,48 @@ const AdminHome = () => {
             <FaBook className="h-8 w-8"></FaBook>
           </div>
           <div className="stat-title">Products</div>
-          <div className="stat-value">{stats?.menuItems}</div>
+          <div className="stat-value">{chartData?.menuItems}</div>
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
-
 
         <div className="stat">
           <div className="stat-figure text-secondary">
             <FaJediOrder className="w-8 h-8"></FaJediOrder>
           </div>
           <div className="stat-title">Orders</div>
-          <div className="stat-value">{stats?.order}</div>
+          <div className="stat-value">{chartData?.order}</div>
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
+      </div>
+      <div className="flex">
+        <div className="w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={stats}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {stats?.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        <div className="w-1/2"></div>
       </div>
     </div>
   );
