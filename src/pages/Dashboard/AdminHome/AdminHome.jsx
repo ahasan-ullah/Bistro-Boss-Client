e4d09a506c8f15721f33940b65117bedb0a027e5
Ahasan Ullah <ahasanullah451@gmail.com>
2025-04-08 18:55:42 +0600

    piechart added

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,12 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
+import { PureComponent } from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaBook, FaDollarSign } from "react-icons/fa";
 import { FaJediOrder, FaUser } from "react-icons/fa6";
 import React from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  Sector,
+  Legend,
+} from "recharts";
 
 const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
 const AdminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
@@ -19,7 +33,7 @@ const AdminHome = () => {
     },
   });
 
-  const { data: chartData=[] } = useQuery({
+  const { data: chartData = [] } = useQuery({
     queryKey: ["order-chartData"],
     queryFn: async () => {
       const res = await axiosSecure.get("/order-stats");
@@ -27,24 +41,55 @@ const AdminHome = () => {
     },
   });
 
-  
-  console.log(chartData)
-  console.log(chartData)
-  
-
   //custom chart shape
   const getPath = (x, y, width, height) => {
-    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
     ${x + width / 2}, ${y}
-    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
     Z`;
   };
-  
+
   const TriangleBar = (props) => {
     const { fill, x, y, width, height } = props;
-  
+
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
   };
+
+  //custom piechart
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const pieChartData=stats?.map(data=>{
+    return {name: data.category, value: data.revenue}
+  })
   return (
     <div>
       <h2 className="text-3xl">
@@ -87,7 +132,7 @@ const AdminHome = () => {
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
       </div>
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <div className="w-1/2">
           <BarChart
             width={500}
@@ -115,7 +160,28 @@ const AdminHome = () => {
             </Bar>
           </BarChart>
         </div>
-        <div className="w-1/2"></div>
+        <div className="w-1/2">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={pieChartData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {pieChartData?.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend></Legend>
+          </PieChart>
+        </div>
       </div>
     </div>
   );
